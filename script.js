271157dcd91d734043ab704112659a491494af61
Harvin Bejarano <harvinbejarano@gmail.com>
2019-09-24 15:46:16 -0500

    4th section

--- script.js
+++ script.js
@@ -1,41 +1,102 @@
-// Lecture: Get And Manipulate On Individual Elements - Part 1
-
-/*
-console.log(document.getElementById('list'));
-
-var el = document.getElementById('list');
-console.log(el);
-console.log(typeof el);
-
-// var el = document.getElementById('LIST');
-// console.log(el);
-
-console.log(el.textContent);
-console.log(el.textContent = 'Hello');
-
-var heading = document.getElementById('heading').textContent;
-console.log(heading);
-
-var ulElement = document.getElementById('list');
-console.log(ulElement.textContent = heading);
-
-console.log(el.innerHTML);
-console.log(el.innerHTML = 'Hello');
-console.log(el.innerHTML = '<h1>Hello</h1>');
-console.log(el.textContent = '<h1>Hello</h1>');
-*/
+const ul = document.getElementById('list'),
+      hide = document.getElementById('hide'),
+      search = document.querySelector('header form input');
 
 
+//****Add Note */
+document.getElementById('add-btn').addEventListener('click', (e) => {
+    e.preventDefault();
+    const inputText = document.getElementById('add-input');
 
+   if (inputText.value !== '') {
+        addRow(inputText.value);
+        inputText.value = '';
+    }
+   
+});
 
+//****Edit,Remove Note */
+ul.addEventListener('click',(e) => {
 
+    if(e.target.classList[1] === 'fa-pencil-square-o'){
+        const parentPar = e.target.parentNode,
+                 input  = parentPar.nextElementSibling,
+                 text  = parentPar.previousElementSibling;
 
+        parentPar.style.display = "none";
+        input.style.display = "block";
+        input.value = text.textContent;
 
+        input.addEventListener('keypress', (e)=>{
+            if(e.keyCode === 13){
+                if(input.value !== ''){
+                    text.textContent = input.value;
+                    input.style.display = "none";
+                    parentPar.style.display = "block";
+                }
+                else{
+                    const li = parentPar.parentNode;
+                    li.parentNode.removeChild(li);
+                }
+            }
+        });
+    }
 
+    if(e.target.classList[1] === 'fa-times'){
+        const Li = e.target.parentNode.parentNode;
+        const ul = Li.parentNode;
+        ul.removeChild(Li);
+    }
+    
+});
 
+//****Hide Notes */
+hide.addEventListener('click', (e)=>{
+    const targetElement = e.target,
+          label = targetElement.previousElementSibling;
 
+    if(hide.checked){
+        ul.style.display = 'none';
+        label.textContent = 'Unhide notes';
+    }
+    else{
+        ul.style.display = 'block';
+        label.textContent = 'Hide notes';
+    }
+});
 
+//****Search */
+search.addEventListener('keyup',(e)=>{
+   const searchChar = e.target.value.toUpperCase();
+   const nodes = document.getElementsByTagName('li');
+   Array.from(nodes).forEach((item)=>{
+       console.log(item.children[0]);
+   });
+   
 
+});
 
+function addRow(text){
+    const p     = document.createElement('p'),
+          pText = document.createTextNode(text),
+          li    = document.createElement('li'),
+          iAdd    = document.createElement('i'),
+          iRemove = document.createElement('i'),
+          icP     = document.createElement('p'),
+          input = document.createElement('input');
+
+    iAdd.className = "fa fa-pencil-square-o";
+    iRemove.className= "fa fa-times";  
+    input.classList.add('edit-note');
+
+    p.appendChild(pText);
+    li.appendChild(p);
+    icP.appendChild(iAdd);
+    icP.appendChild(iRemove);
+    li.appendChild(icP)
+    input.setAttribute('type','text');
+    li.appendChild(input);
+    ul.appendChild(li);
+}
 
 
